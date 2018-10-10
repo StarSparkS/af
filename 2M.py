5f339e0680a23a39f6e4f7fb38ccb4f264bf1a65
Jay Pinnamaneni <jayatej.pinnamaneni.civ@mail.mil>
2018-10-10 16:56:15 -0500

    updated wt1

--- 2M.py
+++ 2M.py
@@ -1,10 +1,11 @@
-import requests, math,os,csv,functools,operator,io,re,sys,csv,argparse, calendar
+import requests, math,os,csv,functools,operator,io,re,sys,csv,argparse, calendar, openpyxl
 from copy import deepcopy
 from collections import defaultdict
 from pprint import pprint as pp
 from datetime import date, timedelta, datetime
 from decimal import *
 from lxml import html
+
 
 from itertools import groupby
 
